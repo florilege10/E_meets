fc975d1291379979cbcb3ab67d8f67d42c001d24
florilege10 <deo.kabuya.20@gmail.com>
2025-03-25 10:57:46 +0200

    Suppression du dossier E_meets

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-set -e  # Arrête l'exécution en cas d'erreur
+set -e  # Arrêter en cas d'erreur
 
-# Installer les dépendances
-pip install -r requirements.txt
+# Vérifier si Python3 et pip sont installés
+if ! command -v python3 >/dev/null 2>&1; then
+  echo "Python3 non trouvé, installation..."
+  apt-get update && apt-get install -y python3 python3-pip
+fi
+
+# Utiliser explicitement pip3
+pip3 install -r requirements.txt
 
 # Collecter les fichiers statiques
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
